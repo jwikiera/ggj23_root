ad739359753c67cbe4d5c783f12f4d24ce28c968
jwikiera <jwikiera@student.42lausanne.ch>
2023-02-03 21:22:19 +0100

    commands are now output in the console

--- godot_project/Scripts/Console.gd
+++ godot_project/Scripts/Console.gd
@@ -5,7 +5,7 @@ var console_width: float
 var console_x: float
 var input_node: LineEdit
 
-var command_history = ["test", "lalalalallala", "hello world"]
+var command_history = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,12 +29,24 @@ func _draw():
 		console_width,
 		get_viewport().size.y)
 	draw_rect(console_rect, Singleton.console_color)
+	draw_console_history()
 	#print("we are drawing lol")
 
 
 func _on_Input_text_entered(new_text: String) -> void:
 	print("Console text entered: %s" % new_text)
+	if len(new_text) == 0:
+		return
 	input_node.clear()
+	
+	command_history.append(new_text)
 	
 	if new_text.to_lower() == "exit":
 		Commands.command_exit()
+
+
+func draw_console_history():
+	var command_height: float = get_viewport().size.y - input_node.rect_size.y - Singleton.console_font.get_height()
+	for i in range(len(command_history)):
+		draw_string(Singleton.console_font, Vector2(console_x, command_height), command_history[len(command_history) - 1 - i])
+		command_height -= (Singleton.console_font.get_height() + 2)
--- godot_project/Singletons/Singleton.gd
+++ godot_project/Singletons/Singleton.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
 
-var console_width = 20 #(percentage)
-var console_color = Color(0, 0, 0)
+var console_width: float = 20 #(percentage)
+var console_color: Color = Color(0, 0, 0)
+var console_font: DynamicFont
+
+
+func _ready():
+	console_font = DynamicFont.new()
+	console_font.font_data = load("res://Fonts/Calculator.ttf")
+	console_font.size = 20
+	print(console_font.size)
