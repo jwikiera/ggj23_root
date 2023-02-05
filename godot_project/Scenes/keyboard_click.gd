399084e35bd803637c741bcb7dcbfc6393386db1
Brandon Gonzalez <bran.gonzalez@outlook.com>
2023-02-05 15:01:45 +0100

    debug clavier boucle infinie

--- godot_project/Scenes/keyboard_click.gd
+++ godot_project/Scenes/keyboard_click.gd
@@ -14,13 +14,17 @@ func _ready():
 	#print(sounds)
 	stream = sounds[0]
 
+func random_cos()->float:
+	seed_key_pitch-=1
+	return int(abs(cos(seed_key_pitch)*10000.0))/10000.0
 
 func play(from_position=0.0):
 	
 	stream = sounds[seed_key_pitch % sounds.size()]
-	pitch_scale = lerp(0.9,1.3, int(abs(cos(seed_key_pitch)*10000.0))/1000.0 )
+	pitch_scale = lerp(0.9,1.3, random_cos())
 	while abs(pitch_scale - last_pitch) < 0.1:
-		pitch_scale = lerp(0.9,1.3, int(abs(cos(seed_key_pitch)*10000.0))/1000.0 )
+		seed_key_pitch-=1
+		pitch_scale = lerp(0.9,1.3, random_cos() )
 	.play(from_position)
 
 func _process(deltat):
